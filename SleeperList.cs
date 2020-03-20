fef72d177772669ae9439363221b6a133c570488
NubbbZ <4251461+NubbbZ@users.noreply.github.com>
2020-03-20 14:53:28 -0400

    Add files via upload

--- SleeperList.cs
+++ SleeperList.cs
@@ -1,40 +1,54 @@
 ﻿using Oxide.Core.Libraries.Covalence;
+using System.Collections.Generic;
 
 namespace Oxide.Plugins
 {
-	[Info("SleeperList", "NubbbZ", "1.1.0")]
+	[Info("Sleeper List", "NubbbZ", "1.2.0")]
 	[Description("Returns the total number of sleepers!")]
 	class SleeperList : CovalencePlugin
 	{
 		private void Init()
 		{
-			permission.RegisterPermission("sleeperlist.use", this); /* Registers the permission to get a list of sleepers! */
+			permission.RegisterPermission("sleeperlist.use", this);
+		}
+
+		protected override void LoadDefaultMessages()
+		{
+			lang.RegisterMessages(new Dictionary<string, string>
+			{
+				["denied"] = "You don't have permission to use that command!",
+				["none"] = "There are currently no sleepers on the server!",
+				["single"] = "There is {0} sleeping player!",
+				["multiple"] = "There are {0} sleeping players!"
+			}, this);
 		}
 
 		[Command("sleepers")]
 		private void TestCommand(IPlayer player, string command, string[] args)
 		{
+			int Sleeper_Count = BasePlayer.sleepingPlayerList.Count;
+
 			if (player.HasPermission("sleeperlist.use"))
 			{
 				if (BasePlayer.sleepingPlayerList.Count == 0)
 				{
-					player.Reply("There are currently no sleepers on the server!");
+					player.Reply(lang.GetMessage("none", this, player.Id));
 				} 
 				else
 				{
 					if (BasePlayer.sleepingPlayerList.Count == 1) 
 					{
-						player.Reply("There is " + BasePlayer.sleepingPlayerList.Count.ToString() + " sleeping player!");
+						player.Reply(string.Format(lang.GetMessage("single", this, player.Id), Sleeper_Count.ToString()));
 					} 
 					else 
 					{
-						player.Reply("There is " + BasePlayer.sleepingPlayerList.Count.ToString() + " sleeping players!");
+						player.Reply(string.Format(lang.GetMessage("multiple", this, player.Id), Sleeper_Count.ToString()));
 					}
 				}
 			}
 			else
 			{
-				player.Reply("You don't have permission to use that command!");
+				player.Reply(lang.GetMessage("denied", this, player.Id));
 			}
 		}
 	}
