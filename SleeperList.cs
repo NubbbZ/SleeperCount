cea76d406735388b62cbbbbbdf1c5ad9cd313ea5
NubbbZ <4251461+NubbbZ@users.noreply.github.com>
2020-03-20 14:53:00 -0400

    Add files via upload

---
+++ SleeperList.cs
@@ -0,0 +1,15 @@
+﻿using Oxide.Core.Libraries.Covalence;
+
+namespace Oxide.Plugins
+{
+	[Info("SleeperList", "NubbbZ", "1.0.0")]
+	[Description("Returns the total number of sleepers!")]
+	class SleeperList : CovalencePlugin
+	{
+		[Command("sleepers")]
+		private void TestCommand(IPlayer player, string command, string[] args)
+		{
+			player.Reply("Total Sleepers: " + BasePlayer.sleepingPlayerList.Count.ToString());
+		}
+	}
+}
